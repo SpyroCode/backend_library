95fc22e54418641442e07b8de9c6e9b1145fd132
Efren Almanza Lamas <37811579+SpyroCode@users.noreply.github.com>
2020-07-07 23:13:38 -0500

    instalacion de CORS y correcion de bloqueo se cambia por la dependencia para las llamadas de Fech se cambia por async await

--- src/index.js
+++ src/index.js
@@ -1,30 +1,35 @@
-require('dotenv').config()
-import express from 'express'
-import bodyParser from 'body-parser'
-import logger from './logger'
-import user from './routes/user'
-import book from './routes/book'
-import order from './routes/order'
-import authentication from './routes/authentication'
-import createStore from './persistence/connection'
+require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import logger from "./logger";
+import user from "./routes/user";
+import book from "./routes/book";
+import order from "./routes/order";
+import authentication from "./routes/authentication";
+import createStore from "./persistence/connection";
 
-const port = 3000;
-const app = express()
-const store = createStore()
+const port = 3500;
+const app = express();
+const store = createStore();
 
-app.use(bodyParser.json())
-app.set('store', store)
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
-  logger.info(req)
-  res.send('Express REST API v1')
-})
+app.set("store", store);
 
-app.use('/login', authentication)
-app.use('/user', user)
-app.use('/book', book)
-app.use('/orders', order)
+app.get("/", function (req, res) {
+  logger.info(req);
+  res.send("Express REST API v1");
+});
 
-app.listen(port, ()=>{
+
+app.use("/login", authentication);
+app.use("/user", user);
+app.use("/book", book);
+app.use("/orders", order);
+
+app.listen(port, () => {
   console.log(`Server running on Port:${port}`);
-})
+});
