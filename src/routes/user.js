d6927385afc1036828ff5e08b7627d1ad9b66ff2
Efren Almanza Lamas <spiralnet.mx@gmail.com>
2020-07-09 17:52:45 -0500

    establecer ruta para actualziar usuario, activo y password

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,97 +1,129 @@
-import express from 'express'
-import { hash } from 'bcrypt'
-import logger from '../logger'
-import auth from '../middleware/auth'
-const router = express.Router()
+import express from "express";
+import { hash } from "bcrypt";
+import logger from "../logger";
+import auth from "../middleware/auth";
+const router = express.Router();
 
 router.use((req, res, next) => {
-  logger.info(req.headers)
-  next()
-}, auth())
+  logger.info(req.headers);
+  next();
+}, auth());
 
-const saltRounds = 10
+const saltRounds = 10;
 
-router.post('/', async (req, res) => {
-  const { name, email, password, role,status } = req.body
-  logger.info(JSON.stringify({ name, email, role }))
-  if(name && email && password && role && status){
-    
-    const store = req.app.get('store')
+router.post("/", async (req, res) => {
+  const { name, email, password, role, status } = req.body;
+  logger.info(JSON.stringify({ name, email, role }));
+  if (name && email && password && role && status) {
+    const store = req.app.get("store");
     const result = await store.Users.create({
       name,
       email,
       password: await hash(password, saltRounds),
       role,
-      status
-    })
-    res.send({ result })
-    res.end()
-  }else{
-    logger.error(`[CREATE USER] No found data, name : ${name} ,email: ${email}, password: ${password},role : ${role},status:${status}`)
-        const message='Faltan datos'
-        res.status(500).send({message})
-        res.end()
+      status,
+    });
+    res.send({ result });
+    res.end();
+  } else {
+    logger.error(
+      `[CREATE USER] No found data, name : ${name} ,email: ${email}, password: ${password},role : ${role},status:${status}`
+    );
+    const message = "Faltan datos";
+    res.status(500).send({ message });
+    res.end();
   }
-})
+});
+router.post("/update/:id", async (req, res) => {
+  const { name, email, password } = req.body;
+  logger.info(JSON.stringify({ name, email }));
+  if (name && email && password) {
+    const store = req.app.get("store");
+    const id = req.params.id;
+    const result = await store.Users.update(
+      {
+        name,
+        email,
+        password: await hash(password, saltRounds),
+        status:'activo'
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    );
+    res.send({ result });
+    res.end();
+  } else {
+    logger.error(
+      `[UPDATE USER] No found data, name : ${name} ,email: ${email}, password: ${password}`
+    );
+    const message = "Faltan datos";
+    res.status(500).send({ message });
+    res.end();
+  }
+});
 
-router.get('/', async (req, res) => {
-  const store = req.app.get('store')
+router.get("/", async (req, res) => {
+  const store = req.app.get("store");
   const result = await store.Users.findAll({
-    attributes: ['id', 'name', 'email', 'role','status']
-  })
-  logger.info(JSON.stringify({ result }))
-  res.send({ result })
-  res.end()
-})
+    attributes: ["id", "name", "email", "role", "status"],
+  });
+  logger.info(JSON.stringify({ result }));
+  res.send({ result });
+  res.end();
+});
 
-router.get('/:id', async (req, res) => {
-  
-  const id=req.params.id
-  const status='activo'
-  const store = req.app.get('store')
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const status = "activo";
+  const store = req.app.get("store");
   const result = await store.Users.findOne({
     where: {
       id,
-      status
+      status,
     },
-    attributes: ['id', 'name', 'email', 'role','status']
-  })
-  logger.info(JSON.stringify({ result }))
-  res.send({ result })
-  res.end()
-})
+    attributes: ["id", "name", "email", "role", "status"],
+  });
+  logger.info(JSON.stringify({ result }));
+  res.send({ result });
+  res.end();
+});
 
-router.post('/baja/:id', async (req, res) => {
-    const id=req.params.id
-    const status='inactivo'
-    const store = req.app.get('store')
-    const result = await store.Users.update(
-      {
-        status,
-      }, {
-        where: {
-          id
-          }
-    })
-    res.send({ result })
-    res.end()
-  
-})
-router.post('/activa/:id', async (req, res) => {
-  const id=req.params.id
-  const status='activo'
-  const store = req.app.get('store')
+router.post("/baja/:id", async (req, res) => {
+  const id = req.params.id;
+  const status = "inactivo";
+  const store = req.app.get("store");
   const result = await store.Users.update(
     {
       status,
-    }, {
+    },
+    {
       where: {
-        id
-        }
-  })
-  res.send({ result })
-  res.end()
+        id,
+      },
+    }
+  );
+  res.send({ result });
+  res.end();
+});
+router.post("/activa/:id", async (req, res) => {
+  const id = req.params.id;
+  const status = "activo";
+  const store = req.app.get("store");
+  const result = await store.Users.update(
+    {
+      status,
+    },
+    {
+      where: {
+        id,
+      },
+    }
+  );
+  res.send({ result });
+  res.end();
+});
 
-})
-
-module.exports = router
+module.exports = router;
